044de013ec35c7bbaacef5bda96647b80b52a586
Rakel Moreira <rakelmoreiramendonca@gmail.com>
2021-09-23 13:55:51 -0300

    post todos

--- README.md
+++ README.md
@@ -15,7 +15,9 @@
 - Should be able to create a new todo
 - Should be able to update a todo
 - Should not be able to update a non existing todo
+- Should be able to mark a todo as done
 - Should be able to delete a todo
+- Should not be able to delete a non existing todo
 
 ---
 
--- src/index.js
+++ src/index.js
@@ -11,11 +11,26 @@ app.use(express.json());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: 'User not found' });
+  }
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
   const { name, username } = request.body;
+
+  const userExists = users.find(user => user.namename === username);
+
+  if(userExists) {
+    return response.status(400).json({ error: 'Username already exists' });
+  }
 
   const user = { 
     id: uuidv4(), 
@@ -29,11 +44,27 @@ app.post('/users', (request, response) => {
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+
+  return response.json(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+
+  const { title, deadline } = request.body;
+
+  const todo = { 
+    id: uuidv4(), 
+    title,
+    done: false, 
+    deadline: new Date(deadline), 
+    created_at: new Date()
+  }
+
+  user.todos.push(todo);
+
+  return response.status(201).json(todo);
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
